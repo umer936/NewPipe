cc9c6ffd7385a73b2b6560a287605c35c35e9d55
Umer Salman <umer936@gmail.com>
2019-09-30 16:16:23 -0500

    Got JaCoCo to run. Also added time to the test-execution.txt file

--- run-tests.sh
+++ run-tests.sh
@@ -3,6 +3,6 @@ rm -rf NewPipe
 git clone https://github.com/umer936/NewPipe
 cd NewPipe
 git remote show origin | grep "Fetch URL:" >test-execution.txt
-echo "SHA: " >> test-execution.txt
+printf "SHA: " >> test-execution.txt
 git rev-parse HEAD >> test-execution.txt
-time ./gradlew test >> test-execution.txt
+( time ./gradlew test ) &> test-execution.txt
